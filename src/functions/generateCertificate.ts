6e89a9c1374f7b068f013fb6be4b1fbb755b2e26
lopessoftec <lopessoftec@gmail.com>
2023-03-08 09:25:00 -0300

    Criando template

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,5 +1,10 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { document } from "../utils/dynamodbClient";
+import { compile } from "handlebars";
+import * as dayjs from "dayjs";
+
+import { join } from "path";
+import { readFileSync } from "fs";
 
 interface ICreateCertificate {
     id: string;
@@ -7,9 +12,26 @@ interface ICreateCertificate {
     grade: string;
 }
 
+interface ITemplate {
+    id: string;
+    name: string;
+    grade: string;
+    medal: string;
+    date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+    // para comecar da raiz do projeto process.cwd()
+    const filePath = join(process.cwd(), "src", "templates", "certificates.hbs");
+
+    const html = readFileSync(filePath, "utf-8");
+
+    return compile(html)(data)
+}
+
 export const handler: APIGatewayProxyHandler = async (event) => {
-    const {id, name, grade} = JSON.parse(event.body) as ICreateCertificate;
-    
+    const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
+
     // Para inserir informações na tabela
     await document.put({
         TableName: "users_certificate",
@@ -29,6 +51,21 @@ export const handler: APIGatewayProxyHandler = async (event) => {
             ":id": id
         }
     }).promise();
+
+    // Pega o path do selo
+    const medalPath = join(process.cwd(), "src", "template", "selo.png");
+    // COnverte de png para base64
+    const medal = readFileSync(medalPath, "base64");
+
+    const data: ITemplate = {
+        name,
+        id,
+        grade,
+        date: dayjs().format("DD/MM/YYYY"),
+        medal
+    }
+
+    const content = await compileTemplate(data);
 
     return {
         statusCode: 201,
